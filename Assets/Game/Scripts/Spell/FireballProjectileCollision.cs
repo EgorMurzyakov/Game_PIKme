c7196e1616f942629e943014234118f49cb9b003
VladimirSkiba <vova.skiba06@gmail.com>
2026-03-12 22:18:22 +0400

    Fierball take damage

--- Assets/Game/Scripts/Spell/FireballProjectileCollision.cs
+++ Assets/Game/Scripts/Spell/FireballProjectileCollision.cs
@@ -10,17 +10,25 @@ public class FireballProjectileCollision : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        // проверяем, что слой попадает в маску
-        if ((hitMask.value & (1 << collision.gameObject.layer)) == 0)
-            return;
+        Debug.Log($"[Fireball] Столкновение с {collision.gameObject.name}, слой: {collision.gameObject.layer}");
 
-        // попытка нанести урон через существующую систему
-        var detector = collision.gameObject.GetComponent<DamageDetector>();
-        if (detector != null)
+        // Проверяем, попал ли в цель
+        if ((hitMask.value & (1 << collision.gameObject.layer)) != 0)
         {
-            detector.GetDamage(damage);
+            Debug.Log($"[Fireball] Наносим урон {damage}");
+
+            var detector = collision.gameObject.GetComponentInParent<DamageDetector>();
+            if (detector != null)
+            {
+                detector.GetDamage(damage);
+            }
+            else
+            {
+                Debug.LogWarning($"[Fireball] DamageDetector НЕ найден на {collision.gameObject.name}");
+            }
         }
 
+        // Уничтожаем фаербол в любом случае
         Destroy(gameObject);
     }
 }
