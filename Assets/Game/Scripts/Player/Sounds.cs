d7fed5c119879f78c863244166b0e31bc12cc176
EgorMurzyakov <EgorM40720@gmail.com>
2026-04-01 18:33:01 +0400

    1.0.13.2
    Улучшенная звуковая система

--- Assets/Game/Scripts/Player/Sounds.cs
+++ Assets/Game/Scripts/Player/Sounds.cs
@@ -1,44 +1,73 @@
 using UnityEngine;
+using System;
+using UnityRandom = UnityEngine.Random; // Псевдоним для Random
 
 public class Sounds : MonoBehaviour
-{
-    [SerializeField] private AudioClip[] sounds;
-
+{    
     private AudioSource audioSrc => GetComponent<AudioSource>();
 
-    //public void PlaySound(int _numb, float voluem = 1f, bool destroyed = false, float p1 = 0.85f, float p2 = 1.2f)
-    //{
-    //    audioSrc.pitch = Random.Range(p1, p2);
-    //    audioSrc.PlayOneShot(sounds[_numb], voluem);
-    //}
+    [SerializeField] private SoundList[] sounds;
+    public GroundType ground = GroundType.Soil;
 
-    public void PlaySoundLow(int _numb)
+    public enum SoundType // !!! ВАЖНО !!! чтобы в инспекторе был тот же порядок
     {
-        float voluem = 0.1f;
-        bool destroyed = false;
-        float p1 = 0.85f;
-        float p2 = 1.2f;
-        audioSrc.pitch = Random.Range(p1, p2);
-        audioSrc.PlayOneShot(sounds[_numb], voluem);
+        Footstep,
+        Dodge,
+        Attack,
+        Hit,
+        Damage,
+        Death,
+        Magic
     }
 
-    public void PlaySoundMedium(int _numb)
+    public enum GroundType
     {
-        float voluem = 0.5f;
-        bool destroyed = false;
-        float p1 = 0.85f;
-        float p2 = 1.2f;
-        audioSrc.pitch = Random.Range(p1, p2);
-        audioSrc.PlayOneShot(sounds[_numb], voluem);
+        Soil,
+        Dirt,
+        Tree,
+        Stone
     }
 
-    public void PlaySoundHight(int _numb)
+    public enum SoundMagic
     {
-        float voluem = 1f;
-        bool destroyed = false;
-        float p1 = 0.85f;
-        float p2 = 1.2f;
-        audioSrc.pitch = Random.Range(p1, p2);
-        audioSrc.PlayOneShot(sounds[_numb], voluem);
+        FireballStart,
+        FireballEnd,
+        TornadoStart,
+        TornadoEnd
     }
+
+    public void PlaySound(SoundType type) // Базовые звуки
+    {
+        if (type == SoundType.Footstep && sounds[(int)type].audioClip.Length != 0)
+        {
+            // Берем вариацию звука в зависимости от типа земли под ногами
+            audioSrc.PlayOneShot(sounds[(int)type].audioClip[(int)ground], sounds[(int)type].voluem); 
+        }
+        else if (sounds[(int)type].audioClip.Length != 0)
+        {
+            // Берем случайную вариацию звука
+            audioSrc.PlayOneShot(sounds[(int)type].audioClip[UnityRandom.Range(0, sounds[(int)type].audioClip.Length)], sounds[(int)type].voluem);
+        }
+    }
+
+    public void PlayMagicSound(SoundMagic magicType) // Магия
+    {
+        if (sounds[(int)SoundType.Magic].audioClip.Length != 0)
+        {
+            audioSrc.PlayOneShot(sounds[(int)SoundType.Magic].audioClip[(int)magicType], sounds[(int)magicType].voluem);
+        }
+    }
+
+    public void SetGroundType(GroundType _tp)
+    {
+        ground = _tp;
+    }
+}
+
+[Serializable]
+public struct SoundList // Для удобной работы в инспекторе
+{
+    public string name;
+    public float voluem;
+    public AudioClip[] audioClip;
 }
