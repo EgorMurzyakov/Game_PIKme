302b4028bd09860ad69d517889c81816e9c1c665
EgorMurzyakov <EgorM40720@gmail.com>
2026-04-01 18:33:01 +0400

    1.0.13.2
    Улучшенная звуковая система

--- Assets/Game/Scripts/Battle/HitDetector.cs
+++ Assets/Game/Scripts/Battle/HitDetector.cs
@@ -3,14 +3,16 @@ using UnityEngine;
 public class HitDetector : MonoBehaviour // Универсальный (и для игрока и для врагов)
 {
     [SerializeField] private GameObject character;
-    [SerializeField] private int weaponDamage; // Сырой урон оружия, пока так (можно оставить (хз)), позже можно брать из Item самого оружия
+    [SerializeField] private int weaponDamage; // Сырой урон оружия, пока так (можно оставить (хз)), позже можно брать из Item самого оружия    
     private ColliderSwitch colliderSwitch;
     private BoxCollider weaponCollider;
+    private Sounds sounds;
 
     public void Start()
     {
         colliderSwitch = character.GetComponent<ColliderSwitch>();
         weaponCollider = GetComponent<BoxCollider>();
+        sounds = character.GetComponent<Sounds>();
 
         colliderSwitch.weaponColliderOn += ColliderOn; // Подписываемся на событие (Уведомление о включении коллайдера)
         colliderSwitch.weaponColliderOff += ColliderOff; // (Уведомление о выключении коллайдера)
@@ -27,6 +29,7 @@ public class HitDetector : MonoBehaviour // Универсальный (и для игрока и для вр
         Debug.Log($"Объект {other.name} вошел в триггер");
 
         other.GetComponent<DamageDetector>().GetDamage(weaponDamage);
+        sounds.PlaySound(Sounds.SoundType.Hit); // Звук попадания по врагу (не нанесения урона)
     }
 
     private void ColliderOn()
