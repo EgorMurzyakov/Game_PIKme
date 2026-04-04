28a1e4d93318f3bfd8e35089554d6f79991f1a7b
EgorMurzyakov <EgorM40720@gmail.com>
2026-04-04 22:48:55 +0400

    1.0.14
    Начал делать систему прокачки (достаточно простую).

--- Assets/Game/Scripts/Inventory/Item/WeaponItem.cs
+++ Assets/Game/Scripts/Inventory/Item/WeaponItem.cs
@@ -5,10 +5,37 @@ using UnityEngine;
 
 public class WeaponItem : ItemScriptableObject
 {
-    public int baceDamage;
+    [SerializeField] private int baceDamage;
+
+    public override ItemScriptableObject Clone()
+    {
+        WeaponItem clone = ScriptableObject.CreateInstance<WeaponItem>();
+
+        // Копируем все поля
+        clone.type = this.type;
+        clone.ItemPrefab = this.ItemPrefab;
+        clone.itemName = this.itemName;
+        clone.itemDescription = this.itemDescription;
+        clone.maximumAmount = this.maximumAmount;
+        clone.icon = this.icon;
+        clone.baceDamage = this.baceDamage;
+
+        return clone;
+    }
 
     public void Start()
     {
         type = ItemType.Weapon;
     }
+
+    public int GetBaceDamage()
+    {
+        return baceDamage;
+    }
+
+    public void SetBaceDamage(int _baceDm)
+    {
+        baceDamage = _baceDm;
+    }
+
 }
